cfae7dcf003bdbe899d5da9f5378d65a3d376924
seny <senymama228@gmail.com>
2020-08-07 15:51:20 +0300

    added main function and game flag

--- Lesson3_Karkas_Igri_na_PyGame/ggg.py
+++ Lesson3_Karkas_Igri_na_PyGame/ggg.py
@@ -1,13 +1,22 @@
 import pygame
 from pygame import *
 
-init()
-display.set_mode((600, 400))
+game_flag = True
 
-while 1:
-    # pass
-    # events содержит список событий
-    events = pygame.event.get()
-    for event in events:
-        if event.type == QUIT:
-            exit('hhhhhhhhh')
+
+def main(game_flag):  # основной метод игры
+    init()
+    display.set_mode((600, 400))
+
+    while game_flag:  # основной цикл игры
+        # pass
+        # events содержит список событий
+        events = pygame.event.get()
+        for event in events:
+            if event.type == QUIT:
+                pygame.quit()
+                game_flag = False
+
+
+if __name__ == '__main__':
+    main(game_flag)
