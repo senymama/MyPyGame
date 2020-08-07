ef714377245e0cc04d7f23fac14a797e92a0fe46
seny <senymama228@gmail.com>
2020-08-07 18:21:21 +0300

    added event_checker

--- Lesson3_Karkas_Igri_na_PyGame/ggg.py
+++ Lesson3_Karkas_Igri_na_PyGame/ggg.py
@@ -1,22 +1,54 @@
-import pygame
-from pygame import *
+import pygame as pg
 
 game_flag = True
 
 
-def main(game_flag):  # основной метод игры
-    init()
-    display.set_mode((600, 400))
+def main():  # основной метод игры
+    global game_flag
 
-    while game_flag:  # основной цикл игры
-        # pass
-        # events содержит список событий
-        events = pygame.event.get()
-        for event in events:
-            if event.type == QUIT:
-                pygame.quit()
-                game_flag = False
+    def event_check(event):
+        global game_flag
+        if event.type == pg.QUIT:
+            game_flag = False
+
+    # здесь определяются константы, классы и функции
+    FPS = 60  # частота обновлени окна
+    gameover_f = False  # флаг конца игры
+    name_win = 'название'  # название ока
+    x, y = 600, 400  # размеры окна
+
+    # здесь происходит инициация, создание объектов и др.
+    pg.init()  # иницилизация библиотек
+    pg.display.set_mode((x, y))  # создание окна
+    pg.display.set_caption(name_win)
+    clock = pg.time.Clock()
+
+    # создание обектов
+    #
+
+    # отображает объекты на экран до цикла
+    pg.display.update()
+
+    # главный цикл
+    while game_flag:
+
+        # задержка
+        clock.tick(FPS)
+
+        # цикл обработки событий
+        for i in pg.event.get():
+            event_check(i)
+
+        # --------
+        # изменение объектов и многое др.
+        f = 0
+        f += 1
+        print(f)
+        # --------
+
+        # обновление экрана
+        pg.display.update()
 
 
 if __name__ == '__main__':
-    main(game_flag)
+    main()
